a59dc848907040c6b1666fa73d035047868c3a13
meganedelux <meganedelux@gmail.com>
2021-05-10 15:57:20 +0900

    Initial Commit

---
+++ CrossingAlert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrossingAlert
+//
+//  Created by Kazuto Nakamura on 2021/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrossingAlert/CrossingAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossingAlertApp.swift
+//  CrossingAlert
+//
+//  Created by Kazuto Nakamura on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct CrossingAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
